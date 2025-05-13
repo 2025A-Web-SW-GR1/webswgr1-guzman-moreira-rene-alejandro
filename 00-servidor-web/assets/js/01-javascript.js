9a039beb50a17ac9d4f3bbc7c35857a8aa0e7dfd
reneguzman7 <reneguzman7@outlook.com>
2025-05-13 17:27:28 -0500

    Add JavaScript and TypeScript files with variable examples and logging

---
+++ 00-servidor-web/assets/js/01-javascript.js
@@ -0,0 +1,11 @@
+console.log("Hola Mundo desde JavaScript");
+// Tipos de variables en JS
+// Mutables
+let nombre = "Juan";
+nombre = "Pedro"; // Cambiando el valor de la variable
+// nombre = 123; // Cambiando el tipo de la variable
+nombre = function() {
+ return 1;
+}
+// Inmutables
+const nombreConstante = "Juan";
---
+++ 00-servidor-web/assets/js/02-typescript.ts
@@ -0,0 +1,8 @@
+console.log("Hola Mundo desde JavaScript");
+// var nombre = "Juan"; // NO VAMOS USAR VAR NUNCA
+let nombres = "Juan"; // let es un bloque de variable
+let nombreTS: string = "Juan"; // TypeScript
+console.log(typeof nombres, "nombres"); // string
+let numero = 10; // number
+let numeroTS: number = 10; // TypeScript
+console.log(typeof numero, "numero"); // number
